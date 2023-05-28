b56eb67860ba1fc91022590fdb5c75ae8d6ed54b
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-05-28 15:50:46 -0500

    initial commit

---
+++ src/main/java/cibertec/edu/pe/Inventariogrupo06/Inventariogrupo06Application.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.Inventariogrupo06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inventariogrupo06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inventariogrupo06Application.class, args);
+	}
+
+}
